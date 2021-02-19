b41031579bb2d55d5043a39a748b3103fcd39693
Nir Shihor <nirshihor@gmail.com>
2021-02-19 12:50:15 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.listen(port, function () {
+  console.log("Server started successfully on port", port);
+});

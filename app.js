68440621934fbe002f80b5968c5223e83b207dd1
Nir Shihor <nirshihor@gmail.com>
2021-04-06 19:50:53 +0100

    t

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const ejs = require("ejs");
-const bodyParser = require("body-parser");
+const path = require('path');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (port == null || port == '') {
   port = 3000;
 }
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.get("/", function (req, res) {
-  res.render("index");
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
-app.get("/heritage", function (req, res) {
-  res.render("heritage");
+app.get('/oldAndNew', function (req, res) {
+  res.render('oldAndNew');
 });
 
 app.listen(port, function () {
-  console.log("Server started successfully on port", port);
+  console.log('Server started successfully on port', port);
 });
